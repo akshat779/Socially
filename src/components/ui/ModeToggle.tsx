daab8d43abe2466e7ba8fd0b92c12fec21f3bc08
Akshat Sharma <akshatsharma779@gmail.com>
2025-03-06 02:08:44 +0530

    Theme updated

--- src/components/ui/ModeToggle.tsx
+++ src/components/ui/ModeToggle.tsx
@@ -8,14 +8,15 @@ export default function ModeToggle(){
     const {theme,setTheme} = useTheme()
 
     return(
-        <Button
-        variant={"outline"}
-        size={"icon"}
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
-            <MoonIcon className="absolute h-[1.2rem]/>"
+        // <Button
+        // variant={"outline"}
+        // size={"icon"}
+        // onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        // >
+        //     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
+        //     <MoonIcon className="absolute h-[1.2rem]/>"
 
-        </Button>
+        // </Button>
+        <h1>Theme</h1>
     )
 }
